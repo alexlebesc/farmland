d5eb8f3c1981ead667ba8234888a5c8a26a5a372
Alex Le Bescond <alex.lebescond@gmail.com>
2018-02-25 21:11:31 +0100

    add fields into plant entity

--- db/src/Entity/Plant.php
+++ db/src/Entity/Plant.php
@@ -16,5 +16,87 @@ class Plant
      */
     private $id;
 
-    // add your own fields
+    private $name;
+
+    private $deciduousEvergreen;
+
+    private $sunShadePreference;
+
+    private $shadeTolerance;
+
+    private $performanceRating;
+
+    private $fertility;
+
+    private $cover;
+
+    private $flowers;
+
+    private $description;
+
+    private $size;
+
+    private $cultivation;
+
+    private $uses;
+
+    private $harvestAndStorage;
+
+    private $cookingProcessing;
+
+    private $secondaryUses;
+
+    private $propagation;
+
+    private $maintenance;
+
+    private $numberOfSeedsPerOunce;
+
+    private $minLegalGerminationRate;
+
+    private $ouncesVolumeSeedPer100SqFt;
+
+    private $shortLongExtraLongGerminationTime;
+
+    private $plantInitiallyInFlatsInBeds;
+
+    private $numberOfPlantsPerFlat;
+
+    private $numberOfFirstFlatsPer100SqFt;
+
+    private $numberOfWeeksInFirstFlat;
+
+    private $depthOfSecondFlatAndSpacing;
+
+    private $numberOfPlantsInSecondFlat;
+
+    private $numberOfSecondFlatsPer100SqFt;
+
+    private $numberOfWeeksInSecondFlat;
+
+    private $inBedSpacing;
+
+    private $maxNumberOfPlantsPer100SqFt;
+
+    private $possibleBiointensiveYieldInPoundsPer100SqFtPlanting;
+
+    private $averageUSYieldInPoundsPer100SqFt;
+
+    private $maxPoundsSeedYieldPer100SqFt;
+
+    private $numberOfYearsToBearing;
+
+    private $numberOfYearsToMaxBearing;
+
+    private $numberOfWeeksInHarvestingPeriod;
+
+    private $timeOfYearToPlant;
+
+    private $poundsConsumedPerYearByAveragePersonInUS;
+
+    private $proteinContentPerPoundInGrams;
+
+    private $calorieContentPerPound;
+
+    private $calciumContentPerPoundInMilligrams;
 }
